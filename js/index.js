7f1c5987ce9bcbb17b96c83260259ebd9178aabd
dkario <dylan.kario@gmail.com>
2014-11-16 20:07:50 -0500

    LAUNCH

---
+++ js/index.js
@@ -0,0 +1,64 @@
+/* TODO: FITVIDS */
+
+
+
+
+
+
+$(function() {
+
+
+
+
+
+
+
+/*
+  $('iframe').each(function() {
+    $f(this).addEvent('ready', ready);
+  });
+  function ready(playerID) {
+    // switch case: for each episode, call specific episode seek functions
+  }
+  player.addEvent('ready', function() {
+    console.log('im ready');
+    player.addEvent('pause', onPause);
+  });
+  function onPause(id){
+    console.log('paused me');
+  }
+*/
+
+
+
+});
+
+
+
+$(function() {
+
+  /* NAV */
+
+  // When 'Episodes' tab selected, expand 'Episodes' and highlight Episode 3
+  $('#nav-eps').on('click', function() {
+    // $('#nav-eps + .submenu').addClass('expand');  // Keep 'Episodes' expanded
+    $('.submenu').find('li.active').removeClass('active'); // Deselect current ep
+    $('#nav-ep3').parent().addClass('active'); // Select Episode 3
+  });
+
+  // When specific episode selected, keep 'Episodes' tab selected and expanded
+  $('.submenu a').on('click', function() {
+    $('.menu').find('li.active').removeClass('active'); // Deselect current ep
+    $('#nav-eps').parent().addClass('active');  // Select 'Episodes'
+    $('#nav-eps + .submenu').addClass('expand'); // Keep 'Episodes' expanded
+  });
+
+  // When new tab selected, deselect current episode and close 'Episodes'
+   $('.menu > li > a:not("#nav-eps")').on('click', function() {
+    $('.submenu').find('li.active').removeClass('active'); // Deselect current ep
+    // ($('#nav-eps + .submenu').removeClass('expand')); // Close 'Episodes' 
+  });
+  
+
+});
+
